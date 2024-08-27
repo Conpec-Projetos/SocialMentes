d025268b8720623de4aaf3a8b9ba572b66cff87b
Gustavo Fernandes de Carvalho <205854@dac.unicamp.br>
2024-08-26 22:31:40 -0300

    Telas: Login Page e Esqueceu Senha; Autenticação para Esquecer Senha

--- social_mentes_app/lib/main.dart
+++ social_mentes_app/lib/main.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
-import 'src/view/components/app_widget.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'src/view/screens/login_page.dart';
 
-void main() {
-  runApp(const AppWidget());
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SocialMentes',
+      home: LoginPage(),
+    );
+  }
 }
