992a3a69c3f1af35db3808ef984c1f0989905eb4
Gustavo Fernandes <gustavo.fernandes@conpec.com.br>
2024-09-25 21:56:44 -0300

    Alterando a main para a página certa

--- social_mentes_app/lib/main.dart
+++ social_mentes_app/lib/main.dart
@@ -11,7 +11,7 @@ class AppWidget extends StatelessWidget{
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Socialmentes",
-      home: InformationPage(),
+      home: CargosPage(),
     );
   }
 }
