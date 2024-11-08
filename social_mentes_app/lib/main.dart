a918d0f5929375021c1866612edfa13c04f0b46f
Gustavo Fernandes <gustavo.fernandes@conpec.com.br>
2024-11-08 17:49:55 -0300

    Adicionando firebase na main

--- social_mentes_app/lib/main.dart
+++ social_mentes_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'src/view/components/app_widget.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 void main() {
   runApp(const AppWidget());
 }
