36c3052aeca1a5aa9786e5298bc2a399f762c61b
Guilherme Henrique da Silva <guilherme.silva@conpec.com.br>
2024-11-18 20:41:16 -0300

    integração com firebase e Testes com as telas

--- social_mentes_app/lib/main.dart
+++ social_mentes_app/lib/main.dart
@@ -2,6 +2,32 @@ import 'package:flutter/material.dart';
 //import 'src/view/components/app_widget.dart';
 import 'src/view/screens/tela4.dart';
 
+import 'package:social_mentes/src/view/screens/psico_navegacao.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(title: "socialmentes", home: PsicoNavegacao());
+  }
+}
+
+/*
+
 void main() {
   runApp(AppWidget());
 }
@@ -13,3 +39,4 @@ class AppWidget extends StatelessWidget {
     home: CreateProtocols());
   }
 }
+*/
