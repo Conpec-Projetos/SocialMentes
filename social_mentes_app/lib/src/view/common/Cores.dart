4bcfe76538433ca7082e0be1c030d156c0b79aaf
Guilherme Henrique da Silva <guilherme.silva@conpec.com.br>
2024-11-18 20:42:52 -0300

    arquivo de cores usadas nas telas

---
+++ social_mentes_app/lib/src/view/common/Cores.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Cores {
+  static const azul0 = Color(0xFFCEE7FF);
+  static const azul1 = Color(0xFF7DB9F0);
+  static const azul2 = Color(0xFF5095D5);
+
+  static const cinza1 = Color(0xFFF4F4F4);
+  static const cinza2 = Color(0xFFD7D7D7);
+  static const cinza3 = Color(0xFFABABAB);
+  static const cinza4 = Color(0xFF727272);
+  static const cinza5 = Color(0xFF454545);
+}
