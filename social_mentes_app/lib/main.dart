202b13a1218e778b8803e7181e491ed637086ba0
Guilherme Henrique da Silva <guilherme.silva@conpec.com.br>
2025-03-23 19:11:41 -0300

    testes

--- social_mentes_app/lib/main.dart
+++ social_mentes_app/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:social_mentes/src/view/screens/login_page.dart';
 import 'firebase_options.dart';
+
+import 'src/view/screens/cargos_page.dart';
 import 'src/view/screens/information_page.dart';
+import 'src/view/screens/sign_up_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,6 +16,8 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
