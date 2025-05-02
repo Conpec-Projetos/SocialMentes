edceda4e8046562ab0e1d56da20a58f495c53803
Guilherme Henrique da Silva <guilherme.silva@conpec.com.br>
2025-05-02 15:28:21 -0300

    responsivo

--- social_mentes_app/lib/screens/login/login_page.dart
+++ social_mentes_app/lib/screens/login/login_page.dart
@@ -34,187 +34,180 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFFF4F4F4),
-      body: Center(
-        child: Column(
-          children: [
-            Container(
+    //Tamanho tela
+    final double screenHeight = MediaQuery.of(context).size.height;
+
+    return Container(
+      color: Color(0xFFF4F4F4),
+      child: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            left: 0,
+            child: Image.asset(
+              "assets/images/loginBG_up.png",
+              scale: 150/((150/844)*screenHeight),
+            )
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: Image.asset(
+              "assets/images/loginBG_down.png",
+              scale: 214/((214/844)*screenHeight),
+            )
+          ),
+          
+          Scaffold(
+            backgroundColor: Colors.transparent,
+            body: SizedBox(
+              height: double.infinity,
               width: double.infinity,
-              height: 100,
-              child: Stack(
-                children: [
-                  Image.asset('assets/images/yellow_ball_up.png'),
-                  Image.asset('assets/images/blue_ball_up.png'),
-                  Image.asset('assets/images/red_ball_up.png'),
-                ],
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.only(top: 50),
-              child: Image.asset(
-                'assets/images/Logo.png',
-                width: 100,
-                height: 100,
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.only(top: 1),
-              child: Text(
-                'bem-vindo',
-                style: GoogleFonts.poppins(
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFF5095D5),
-                ),
-              ),
-            ),
-            Container(
-              child: Text(
-                'Sentimos sua falta!',
-                style: GoogleFonts.firaSans(
-                  fontSize: 17,
-                  color: Color(0xFFABABAB),
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: TextFormField(
-                controller: _emailController,
-                autofocus: true,
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.email, color: Color(0xFF727272)),
-                  labelText: 'Email',
-                  labelStyle: GoogleFonts.firaSans(
-                    fontSize: 20,
-                    color: Color(0xFF727272),
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Color(0xFF727272)),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Color(0xFF5095D5)),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 10),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: TextFormField(
-                controller: _passwordController,
-                keyboardType: TextInputType.text,
-                obscureText: !_passwordVisible,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.lock, color: Color(0xFF727272)),
-                  labelText: 'Senha',
-                  labelStyle: GoogleFonts.firaSans(
-                      fontSize: 20, color: Color(0xFF727272)),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Color(0xFF727272)),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Color(0xFF5095D5)),
-                  ),
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      _passwordVisible
-                          ? Icons.visibility_off
-                          : Icons.visibility,
-                      color: Color(0xFF727272),
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        _passwordVisible = !_passwordVisible;
-                      });
-                    },
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              width: 150,
-              height: 70,
-              child: GestureDetector(
-                onTap: _loginUser,
-                child: OverflowBox(
-                  minWidth: 0.0,
-                  minHeight: 0.0,
-                  maxWidth: double.infinity,
-                  maxHeight: double.infinity,
-                  child: Image.asset('assets/images/Button.png'),
-                ),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.only(bottom: 10),
-              child: Row(
+              child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text(
-                    'Esqueceu sua senha?',
-                    style: GoogleFonts.firaSans(
-                      fontSize: 15,
-                      color: Color(0xFF727272),
+                  Container(
+                    margin: EdgeInsets.only(top: (173/844)*screenHeight),
+                    child: Image.asset(
+                      'assets/images/logo_grande.png',
+                      width: 100,
+                      height: 100,
                     ),
                   ),
-                  SizedBox(width: 10),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => PasswordPage(),
-                        ),
-                      );
-                    },
+                  Container(
+                    padding: EdgeInsets.only(top: 1),
                     child: Text(
-                      'Clique aqui!',
-                      style: GoogleFonts.firaSans(
-                        fontSize: 15,
-                        color: Color(0xFF5095D5),
+                      'bem-vindo',
+                      style: GoogleFonts.poppins(
+                        fontSize: 40,
                         fontWeight: FontWeight.bold,
+                        color: Color(0xFF5095D5),
                       ),
                     ),
-                  )
+                  ),
+                  Text(
+                    'Sentimos sua falta!',
+                    style: GoogleFonts.firaSans(
+                      fontSize: 17,
+                      color: Color(0xFFABABAB),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: TextFormField(
+                      controller: _emailController,
+                      autofocus: true,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.email, color: Color(0xFF727272)),
+                        labelText: 'Email',
+                        labelStyle: GoogleFonts.firaSans(
+                          fontSize: 20,
+                          color: Color(0xFF727272),
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Color(0xFF727272)),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Color(0xFF5095D5)),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: TextFormField(
+                      controller: _passwordController,
+                      keyboardType: TextInputType.text,
+                      obscureText: !_passwordVisible,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.lock, color: Color(0xFF727272)),
+                        labelText: 'Senha',
+                        labelStyle: GoogleFonts.firaSans(
+                            fontSize: 20, color: Color(0xFF727272)),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Color(0xFF727272)),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Color(0xFF5095D5)),
+                        ),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            _passwordVisible
+                                ? Icons.visibility_off
+                                : Icons.visibility,
+                            color: Color(0xFF727272),
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _passwordVisible = !_passwordVisible;
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    width: 150,
+                    height: 70,
+                    child: GestureDetector(
+                      onTap: _loginUser,
+                      child: OverflowBox(
+                        minWidth: 0.0,
+                        minHeight: 0.0,
+                        maxWidth: double.infinity,
+                        maxHeight: double.infinity,
+                        child: Image.asset('assets/images/Button.png'),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(bottom: 10),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Esqueceu sua senha?',
+                          style: GoogleFonts.firaSans(
+                            fontSize: 15,
+                            color: Color(0xFF727272),
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => PasswordPage(),
+                              ),
+                            );
+                          },
+                          child: Text(
+                            'Clique aqui!',
+                            style: GoogleFonts.firaSans(
+                              fontSize: 15,
+                              color: Color(0xFF5095D5),
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
-            SizedBox(height: 75.25),
-            Container(
-              width: double.infinity,
-              height: 200,
-              child: Stack(
-                children: [
-                  Positioned(
-                    right: -25,
-                    top: -10,
-                    child: Image.asset('assets/images/yellow_ball_down.png'),
-                  ),
-                  Positioned(
-                    top: 139,
-                    right: -40,
-                    child: Image.asset('assets/images/blue_ball_down.png'),
-                  ),
-                  Positioned(
-                    right: 0,
-                    top: 120,
-                    child: Image.asset('assets/images/red_ball_down.png'),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
