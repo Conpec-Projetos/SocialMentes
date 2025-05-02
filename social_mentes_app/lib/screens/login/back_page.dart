9d2ac1ed513c940f9a78e24e3c78e7ec95f9c50d
Guilherme Henrique da Silva <guilherme.silva@conpec.com.br>
2025-05-02 15:25:53 -0300

    tela de login responsiva

--- social_mentes_app/lib/screens/login/back_page.dart
+++ social_mentes_app/lib/screens/login/back_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'login_page.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class BackPage extends StatelessWidget {
@@ -7,94 +6,81 @@ class BackPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFFF4F4F4),
-      body: Center(
-        child: Column(
-          children: [
-            Container(
+    //Tamanho tela
+    final double screenHeight = MediaQuery.of(context).size.height;
+
+    return Container(
+      color: Color(0xFFF4F4F4),
+      child: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            left: 0,
+            child: Image.asset(
+              "assets/images/loginBG_up.png",
+              scale: 150/((150/844)*screenHeight),
+            )
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: Image.asset(
+              "assets/images/loginBG_down.png",
+              scale: 214/((214/844)*screenHeight),
+            )
+          ),
+          Scaffold(
+            backgroundColor: Colors.transparent,
+            body: SizedBox(
+              height: double.infinity,
               width: double.infinity,
-              height: 100,
-              child: Stack(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Image.asset('assets/images/yellow_ball_up.png'),
-                  Image.asset('assets/images/blue_ball_up.png'),
-                  Image.asset('assets/images/red_ball_up.png'),
-                ],
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.only(top: 50),
-              child: Image.asset(
-                'assets/images/Logo.png',
-                width: 100,
-                height: 100,
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.only(top: 1),
-              child: Text(
-                'Verfique seu e-mail',
-                style: GoogleFonts.poppins(
-                  fontSize: 35,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFF5095D5),
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
-            Container(
-              child: Text(
-                'Enviamos um link para redefinir sua senha',
-                style: GoogleFonts.firaSans(
-                  fontSize: 18,
-                  color: Color(0xFFABABAB),
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              style:
-                  ElevatedButton.styleFrom(backgroundColor: Color(0xFF5095D5)),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => LoginPage(),
+                  Container(
+                    margin: EdgeInsets.only(top: (173/844)*screenHeight),
+                    child: Image.asset(
+                      'assets/images/logo_grande.png',
+                      width: 100,
+                      height: 100,
+                    ),
                   ),
-                );
-              },
-              child: Text(
-                'Voltar a tela de login',
-                style: GoogleFonts.poppins(color: Colors.white),
-              ),
-            ),
-            SizedBox(height: 244.2),
-            Container(
-              width: double.infinity,
-              height: 200,
-              child: Stack(
-                children: [
-                  Positioned(
-                    right: -25,
-                    top: -10,
-                    child: Image.asset('assets/images/yellow_ball_down.png'),
+                  Container(
+                    padding: EdgeInsets.only(top: 1),
+                    child: Text(
+                      'Verfique seu e-mail',
+                      style: GoogleFonts.poppins(
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF5095D5),
+                      ),
+                    ),
                   ),
-                  Positioned(
-                    top: 139,
-                    right: -40,
-                    child: Image.asset('assets/images/blue_ball_down.png'),
+                  SizedBox(height: 20),
+                  Text(
+                    'Enviamos um link para redefinir sua senha',
+                    style: GoogleFonts.firaSans(
+                      fontSize: 18,
+                      color: Color(0xFFABABAB),
+                    ),
                   ),
-                  Positioned(
-                    right: 0,
-                    top: 120,
-                    child: Image.asset('assets/images/red_ball_down.png'),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Color(0xFF5095D5)),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      'Voltar a tela de login',
+                      style: GoogleFonts.poppins(color: Colors.white),
+                    ),
                   ),
                 ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- social_mentes_app/lib/screens/login/forget_password_page.dart
+++ social_mentes_app/lib/screens/login/forget_password_page.dart
@@ -19,113 +19,110 @@ class PasswordPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFFF4F4F4),
-      body: Center(
-        child: Column(
-          children: [
-            Container(
+    //Tamanho tela
+    final double screenHeight = MediaQuery.of(context).size.height;
+
+    return Container(
+      color: Color(0xFFF4F4F4),
+      child: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            left: 0,
+            child: Image.asset(
+              "assets/images/loginBG_up.png",
+              scale: 150/((150/844)*screenHeight),
+            )
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: Image.asset(
+              "assets/images/loginBG_down.png",
+              scale: 214/((214/844)*screenHeight),
+            )
+          ),
+          Scaffold(
+            backgroundColor: Colors.transparent,
+            body: SizedBox(
+              height: double.infinity,
               width: double.infinity,
-              height: 100,
-              child: Stack(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Image.asset('assets/images/yellow_ball_up.png'),
-                  Image.asset('assets/images/blue_ball_up.png'),
-                  Image.asset('assets/images/red_ball_up.png'),
+                  Container(
+                    margin: EdgeInsets.only(top: (173/844)*screenHeight),
+                    child: Image.asset(
+                      'assets/images/logo_grande.png',
+                      width: 100,
+                      height: 100,
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(top: 1),
+                    child: Text(
+                      'Recuperar Senha',
+                      style: GoogleFonts.poppins(
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF5095D5),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Text(
+                      'Sentimos sua falta!',
+                      style: GoogleFonts.firaSans(
+                        fontSize: 17,
+                        color: Color(0xFFABABAB),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: TextFormField(
+                      controller: _emailController,
+                      autofocus: true,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.email, color: Color(0xFF727272)),
+                        labelText: 'Email',
+                        labelStyle: GoogleFonts.firaSans(
+                          fontSize: 20,
+                          color: Color(0xFF727272),
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Color(0xFF727272)),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Color(0xFF5095D5)),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFF5095D5)),
+                    onPressed:() async{
+                        _resetPassword();
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => BackPage()))
+                      .then((response){
+                        Navigator.pop(context);
+                      });
+                    } ,
+                    child: Text("Enviar", 
+                    style:GoogleFonts.poppins(color: Colors.white),
+                    ),
+                  ),
                 ],
               ),
             ),
-            Container(
-              padding: EdgeInsets.only(top: 50),
-              child: Image.asset(
-                'assets/images/Logo.png',
-                width: 100,
-                height: 100,
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.only(top: 1),
-              child: Text(
-                'Recuperar Senha',
-                style: GoogleFonts.poppins(
-                  fontSize: 35,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFF5095D5),
-                ),
-              ),
-            ),
-            Container(
-              child: Text(
-                'Sentimos sua falta!',
-                style: GoogleFonts.firaSans(
-                  fontSize: 17,
-                  color: Color(0xFFABABAB),
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: TextFormField(
-                controller: _emailController,
-                autofocus: true,
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.email, color: Color(0xFF727272)),
-                  labelText: 'Email',
-                  labelStyle: GoogleFonts.firaSans(
-                    fontSize: 20,
-                    color: Color(0xFF727272),
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Color(0xFF727272)),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Color(0xFF5095D5)),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xFF5095D5)),
-                onPressed:() async{
-                   _resetPassword();
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => BackPage()));
-                } ,
-                child: Text("Enviar", 
-                style:GoogleFonts.poppins(color: Colors.white),
-                ),
-                ),
-            SizedBox(height: 187.2),
-            Container(
-              width: double.infinity,
-              height: 200,
-              child: Stack(
-                children: [
-                  Positioned(
-                    right: -25,
-                    top: -10,
-                    child: Image.asset('assets/images/yellow_ball_down.png'),
-                  ),
-                  Positioned(
-                    top: 139,
-                    right: -40,
-                    child: Image.asset('assets/images/blue_ball_down.png'),
-                  ),
-                  Positioned(
-                    right: 0,
-                    top: 120,
-                    child: Image.asset('assets/images/red_ball_down.png'),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
